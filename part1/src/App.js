eb5c8d62a836782124b9fa7c7a84f52877b41149
Sebastian Lundin <seblundin@gmail.com>
2021-12-18 11:09:05 +0200

    part 1 a

--- part1/src/App.js
+++ part1/src/App.js
@@ -9,19 +9,40 @@ function App() {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3}
+      exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total sum={exercises1+exercises2+exercises3}/>
     </div>
   )
+}
+
+function Header(props) {
+  return (
+    <h1>{props.course}</h1>
+  );
+}
+
+function Content(props) {
+  return (
+    <>
+      <Part part={props.part1} exercise={props.exercises1}/>
+      <Part part={props.part2} exercise={props.exercises2}/>
+      <Part part={props.part3} exercise={props.exercises3}/>
+    </>
+  );
+}
+
+function Part(props) {
+  return(
+    <p>{props.part} {props.exercise}</p>
+  );
+}
+
+function Total(props) {
+  return (
+    <p>Number of exercises {props.sum}</p>
+  );
 }
 
 export default App;
